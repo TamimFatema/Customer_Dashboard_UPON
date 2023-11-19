82973db922ac68b910eadfff5d575c3e4284cce4
Tamim Fatema <tamim.fatema24@gmail.com>
2023-11-19 11:41:31 +0600

    structure

--- customer-dashboard/src/App.jsx
+++ customer-dashboard/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Sidebar from "./Pages/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Sidebar />
     </>
   );
 }
---
+++ customer-dashboard/src/Pages/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Sidebar/Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Sidebar
