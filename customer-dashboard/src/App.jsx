6dc4d7a695e977658af2c218d14bf0af1216d7f7
Tamim Fatema <tamim.fatema24@gmail.com>
2023-11-19 15:58:08 +0600

    Filing Structure and Sidebar design initial

--- customer-dashboard/src/App.jsx
+++ customer-dashboard/src/App.jsx
@@ -1,11 +1,15 @@
-import "./App.css";
+// src/App.js
+import React from "react";
 import Sidebar from "./Pages/Sidebar/Sidebar";
 
 function App() {
   return (
-    <>
+    <div className="flex">
       <Sidebar />
-    </>
+      {/* Add the rest of your content here */}
+      <div className="flex-1 p-4">
+      </div>
+    </div>
   );
 }
 
